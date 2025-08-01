af3693f9c52dfa3795ca96a0253893e928c6c464
Saleh Rafi <salehrafishyam@gmail.com>
2025-08-01 15:22:07 +0600

    Important features added

---
+++ logic/firebase_conn.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqnfjbcw6VML5dQQdFVIXi_MRu3UvbM44",
+    authDomain: "gen-2-ngl.firebaseapp.com",
+    projectId: "gen-2-ngl",
+    storageBucket: "gen-2-ngl.firebasestorage.app",
+    messagingSenderId: "174007267031",
+    appId: "1:174007267031:web:ed8ee57c0f58802c468a92",
+    measurementId: "G-2LE3HCMZ6X"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
